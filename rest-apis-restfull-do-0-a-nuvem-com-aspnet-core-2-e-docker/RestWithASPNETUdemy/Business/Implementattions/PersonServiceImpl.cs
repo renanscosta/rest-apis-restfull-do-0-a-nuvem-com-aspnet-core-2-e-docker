a678d846a994a310e8d42d3eb59c0ae5eadc8fa2
Renan <renan.costa.ti@gmail.com>
2020-07-04 12:59:49 -0300

    adição do projeto

---
+++ rest-apis-restfull-do-0-a-nuvem-com-aspnet-core-2-e-docker/RestWithASPNETUdemy/Business/Implementattions/PersonServiceImpl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+
+        private IPersonRepository _repository;
+
+        public PersonBusinessImpl(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // Método responsável por retornar uma pessoa
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ rest-apis-restfull-do-0-a-nuvem-com-aspnet-core-2-e-docker/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementattions;
+using RestWithASPNETUdemy.Repository.Implementattions;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public IConfiguration _configuration { get; }
+        public readonly ILogger _logger;
+        public IHostingEnvironment _environment {get;}
+
+        
+        public Startup(IConfiguration configuration, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            _configuration = configuration;
+            _environment = env;
+            _logger = logger;
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = _configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connectionString));
+            
+            if(_environment.IsDevelopment())
+            {
+                try
+                {
+                    var envolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+                    var evolve = new Evolve.Evolve("evolve.json",envolveConnection,msg => _logger.LogInformation(msg))
+                    {
+                        Locations = new List<string>{ "db/migrations" },
+                        IsEraseDisabled = true
+                    };
+
+                    evolve.Migrate();
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogCritical("Database migration failed.",ex);
+                }
+            }
+
+            //Dependency Injection
+            services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
+            services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
+            services.AddApiVersioning();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(_configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
